1cfb5bfdf8f7cb9e1035cb8e5e93150b98d5eff6
刘磊 <liulei901112@163.com>
2023-02-28 23:49:31 +0800

    描述

--- FindWiFiPassword/Properties/AssemblyInfo.cs
+++ FindWiFiPassword/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("FindWiFiPassword")]
+[assembly: AssemblyTitle("查找本地连接过的WIFI密码")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
